2b0acdb5567bab3950d3a19ed35983566525f8f6
Manuel Pinto <manuel11.pinto11@gmail.com>
2022-01-30 22:44:30 +0100

    update script.js, game functionality complete

--- firstProject/script.js
+++ firstProject/script.js
@@ -2,5 +2,32 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
 
+const generateTarget = () => Math.floor(Math.random() * 10);
+
+const getAbsoluteDistance = (num1, num2) => Math.abs(num1 - num2);
+
+const compareGuesses = (userGuess, computerGuess, targetNumber) => {
+  if (userGuess < 0 || userGuess > 9 || computerGuess < 0 || computerGuess > 9) {
+    alert("You didn't pick a number between 0 and 9");
+    return false;
+  }
+  const userGuessHowOff = getAbsoluteDistance(userGuess, targetNumber);
+  const computerGuessHowOff = getAbsoluteDistance(computerGuess, targetNumber);
+  if (userGuessHowOff <= computerGuessHowOff) 
+    return true
+  else 
+    return false
+};
+
+const updateScore = (winner) => {
+  if (winner = 'human') 
+    humanScore++;
+  if (winner = 'computer')
+    computerScore++;
+};
+
+const advanceRound = () => currentRoundNumber++;
